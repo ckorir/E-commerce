a1d19713f91d51341f041881c0dc82d34aa54462
ckorir <chrisruto.k@gmail.com>
2024-06-14 13:58:06 +0300

    Added state for menu and fonts

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react';
 import '../css/Navbar.css'
 
 import logo from '../components/Assets/logo.png'
@@ -6,23 +6,31 @@ import cart_icon from '../components/Assets/cart_icon.png'
 
 
 export default function Navbar() {
+
+  const [menu,setMenu] = useState("shop");
+
   return (
+
     <div className="navbar">
+
       <div className="nav-logo">
         <img src={logo} alt="logo" />
         <p>SHOPPER</p>
       </div>
+
       <ul className="nav-menu">
-        <li>Shop<hr/></li>
-        <li>Men</li>
-        <li>Women</li>
-        <li>Kids</li>
+        <li onClick={()=>{setMenu("shop")}}>Shop{menu==="shop"?<hr/>:<></>}</li>
+        <li onClick={()=>{setMenu("mens")}}>Men{menu==="mens"?<hr/>:<></>}</li>
+        <li onClick={()=>{setMenu("womens")}}>Women{menu==="womens"?<hr/>:<></>}</li>
+        <li onClick={()=>{setMenu("kids")}}>Kids{menu==="kids"?<hr/>:<></>}</li>
       </ul>
+
       <div className="nav-login-cart">
         <button>Login</button>
         <img src={cart_icon} alt="cart_icon" />
-        <div className='nav-cart-count'>0</div>
+        <div className="nav-cart-count">0</div>
       </div>
+
     </div>
   )
 }
