f33aebc0a5cbd00752b388c0365af0e4c16e9041
ckorir <chrisruto.k@gmail.com>
2024-06-14 13:32:22 +0300

    Created navbar and styling for it

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,10 +1,28 @@
 import React from 'react'
 import '../css/Navbar.css'
 
+import logo from '../components/Assets/logo.png'
+import cart_icon from '../components/Assets/cart_icon.png'
+
+
 export default function Navbar() {
   return (
-    <div className="Navbar">
-      Navbar
+    <div className="navbar">
+      <div className="nav-logo">
+        <img src={logo} alt="logo" />
+        <p>SHOPPER</p>
+      </div>
+      <ul className="nav-menu">
+        <li>Shop<hr/></li>
+        <li>Men</li>
+        <li>Women</li>
+        <li>Kids</li>
+      </ul>
+      <div className="nav-login-cart">
+        <button>Login</button>
+        <img src={cart_icon} alt="cart_icon" />
+        <div className='nav-cart-count'>0</div>
+      </div>
     </div>
   )
 }
