0c2828b53c1f66f167de5e1c335fa639356da40d
ckorir <chrisruto.k@gmail.com>
2024-06-15 13:08:56 +0300

    Added links to the navbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -3,6 +3,7 @@ import '../css/Navbar.css'
 
 import logo from '../components/Assets/logo.png'
 import cart_icon from '../components/Assets/cart_icon.png'
+import { Link } from 'react-router-dom';
 
 
 export default function Navbar() {
@@ -19,15 +20,15 @@ export default function Navbar() {
       </div>
 
       <ul className="nav-menu">
-        <li onClick={()=>{setMenu("shop")}}>Shop{menu==="shop"?<hr/>:<></>}</li>
-        <li onClick={()=>{setMenu("mens")}}>Men{menu==="mens"?<hr/>:<></>}</li>
-        <li onClick={()=>{setMenu("womens")}}>Women{menu==="womens"?<hr/>:<></>}</li>
-        <li onClick={()=>{setMenu("kids")}}>Kids{menu==="kids"?<hr/>:<></>}</li>
+        <li onClick={()=>{setMenu("shop")}}><Link to="/">Shop</Link>{menu==="shop"?<hr/>:<></>}</li>
+        <li onClick={()=>{setMenu("mens")}}><Link to="/mens">Men</Link>{menu==="mens"?<hr/>:<></>}</li>
+        <li onClick={()=>{setMenu("womens")}}><Link to="/womens">Women</Link>{menu==="womens"?<hr/>:<></>}</li>
+        <li onClick={()=>{setMenu("kids")}}><Link to="/kids">Kids</Link>{menu==="kids"?<hr/>:<></>}</li>
       </ul>
 
       <div className="nav-login-cart">
-        <button>Login</button>
-        <img src={cart_icon} alt="cart_icon" />
+        <Link to="/login"><button>Login</button></Link>
+        <Link to="/cart"><img src={cart_icon} alt="cart_icon" /></Link>
         <div className="nav-cart-count">0</div>
       </div>
 
