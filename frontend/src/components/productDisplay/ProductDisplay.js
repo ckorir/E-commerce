1c77bbd1d3a4ef4d999b9c20481627693081772c
ckorir <chrisruto.k@gmail.com>
2024-07-08 11:08:01 +0300

    Code for product display

--- frontend/src/components/productDisplay/ProductDisplay.js
+++ frontend/src/components/productDisplay/ProductDisplay.js
@@ -1,8 +1,64 @@
 import React from 'react'
 import '../../css/ProductDisplay.css'
+import star_icon from '../Assets/star_icon.png'
+import stardull_icon from '../Assets/star_dull_icon.png'
 
-export default function ProductDisplay() {
+export default function ProductDisplay(props) {
+    const {product} = props;
   return (
-    <div className='productdisplay'>ProductDisplay</div>
+    <div className='productdisplay'>
+
+        <div className='productdisplay-left'>
+            <div className='productdisplay-img-list'>
+                <img src={product.image} alt='' />
+                <img src={product.image} alt='' />
+                <img src={product.image} alt='' />
+                <img src={product.image} alt='' />
+            </div>
+            <div className='productdisplay-img'>
+                <img className='productdisplay-main-img' src={product.image} alt='' />
+            </div>
+        </div> 
+
+        <div className='productdisplay-right'>
+            <h1>{product.name}</h1>
+            <div className='productdisplay-right-star'>
+                <img src={star_icon} alt='' />
+                <img src={star_icon} alt='' />
+                <img src={star_icon} alt='' />
+                <img src={star_icon} alt='' />
+                <img src={stardull_icon} alt='' />
+                <p>(4.5)</p>
+            </div>
+
+            <div className='productdisplay-right-price'>
+                <div className='productdisplay-right-price-old'>
+                    ${product.old_price}
+                </div>
+                <div className='productdisplay-right-price-new'>
+                    ${product.new_price}
+                </div>
+                <div className='productdisplay-right-description'>
+                In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. 
+                </div>
+
+                <div className='productdisplay-right-size'>
+                    <h1>Select Size</h1>
+                    <div className='productdisplay-right-sizes'>
+                        <div>S</div>
+                        <div>M</div>
+                        <div>L</div>
+                        <div>XL</div>
+                        <div>XXL</div>
+                    </div>
+                    <button>Add to Cart</button>
+                    <p className='productdisplay-right-category'><span>Category: </span>Women, T-shirt, Crop Top</p>
+                    <p className='productdisplay-right-category'><span>Tags: </span>Modern, Latest</p>
+                </div>
+
+            </div>
+        </div>
+
+    </div>
   )
 }
