d1b73f93497554647d6dd75a6c5eb852eefc44c8
ckorir <chrisruto.k@gmail.com>
2024-07-08 10:14:19 +0300

    Added code for login

--- frontend/src/pages/LoginSingup.js
+++ frontend/src/pages/LoginSingup.js
@@ -1,7 +1,23 @@
 import React from 'react'
+import '../css/LoginSingup.css'
 
 export default function LoginSingup() {
   return (
-    <div>LoginSingup</div>
+    <div className='loginsingup'>
+      <div className='loginsingup-container'>
+        <h1>Sign Up</h1>
+        <div className='loginsingup-fields'>
+          <input type="text" placeholder='Username' />
+          <input type="email" placeholder='Email Adress' />
+          <input type="password" placeholder='Password' />
+          <button>Continue</button>
+          <p className='loginsingup-login'>Already have an account?<span> Login </span></p>
+          <div className='loginsingup-agree'>
+            <input type="checkbox" name='' id='' />
+            <p>I agree to the <span>Terms & Conditions</span></p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
