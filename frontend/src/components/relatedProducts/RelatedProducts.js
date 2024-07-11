8f21cad1974f077dbd7f7f6d7a07d7fd5b3af5d5
ckorir <chrisruto.k@gmail.com>
2024-07-11 17:36:01 +0300

    Code correction

---
+++ frontend/src/components/relatedProducts/RelatedProducts.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../../css/RelatedProducts.css'
+
+export default function RelatedProducts() {
+  return (
+    <div className='relatedproducts'>
+        <h1>Related products</h1>
+    </div>
+  )
+}
