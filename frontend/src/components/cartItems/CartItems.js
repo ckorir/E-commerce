8d00a7964eae8d16d352bb1dbd5ea5bd789dc648
ckorir <chrisruto.k@gmail.com>
2024-07-14 11:39:04 +0300

    Added code for cart to display items added to cart

---
+++ frontend/src/components/cartItems/CartItems.js
@@ -0,0 +1,44 @@
+import React, { useContext } from 'react';
+import '../../css/CartItems.css';
+import { ShopContext } from '../../context/Context';
+import remove_icon from '../../components/Assets/cart_cross_icon.png';
+
+export default function CartItems() {
+
+    const { all_product, cartItems, removeFromCart } = useContext(ShopContext);
+
+    return (
+        <div className='cartitems'>
+            <div className='cartitems-format-main'>
+                <p>Products</p>
+                <p>Title</p>
+                <p>Price</p>
+                <p>Quantity</p>
+                <p>Total</p>
+                <p>Remove</p>
+            </div>
+            <hr />
+
+            {all_product.map((e) => {
+                if (cartItems[e.id] > 0) {
+                    return (
+                        <div key={e.id}>
+                            <div className='cartitems-format'>
+                                <img className='carticon-product-icon' src={e.image} alt='' />
+                                <p>{e.name}</p>
+                                <p>${e.new_price}</p>
+                                <button className='cartitems-quantity'>{cartItems[e.id]}</button>
+                                <p>{e.new_price * cartItems[e.id]}</p>
+                                <img onClick={() => { removeFromCart(e.id) }} className='carticon-remove-icon' src={remove_icon} alt='' />
+                            </div>
+                            <hr />
+                        </div>
+                    );
+                } else {
+                    return null;
+                }
+            })}
+
+        </div>
+    );
+}
--- frontend/src/pages/Cart.js
+++ frontend/src/pages/Cart.js
@@ -1,7 +1,11 @@
 import React from 'react'
+import '../css/Cart.css'
+import CartItems from '../components/cartItems/CartItems'
 
 export default function Cart() {
   return (
-    <div>Cart</div>
+    <div className='cart'>
+      <CartItems/>
+    </div>
   )
 }
