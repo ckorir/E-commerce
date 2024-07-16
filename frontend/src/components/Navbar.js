8480e5f5bb63f1bf786e0a7476444bd91aebe5ba
ckorir <chrisruto.k@gmail.com>
2024-07-16 11:47:09 +0300

    Styling for dropdown icon on media 800

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,10 +1,11 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useRef } from 'react';
 import '../css/Navbar.css'
 
 import logo from '../components/Assets/logo.png'
 import cart_icon from '../components/Assets/cart_icon.png'
 import { Link } from 'react-router-dom';
 import { ShopContext } from '../context/Context';
+import nav_dropdown from '../components/Assets/nav_dropdown.png'
 
 
 export default function Navbar() {
@@ -12,6 +13,11 @@ export default function Navbar() {
   const [menu,setMenu] = useState("shop");
   const {getTotalCartItems} = useContext(ShopContext);
   const menuRef = useRef();
+
+  const dropdown_toggle = (e) => {
+    menuRef.current.classList.toggle("nav-menu-visible");
+    e.target.classList.toggle("open");
+  }
 
   return (
 
@@ -22,7 +28,7 @@ export default function Navbar() {
         <p>SHOPPER</p>
       </div>
 
-      {/* <img src={menu} alt="" /> */}
+      <img className='nav-dropdown' onClick={dropdown_toggle} src={nav_dropdown} alt="" />
       <ul ref={menuRef} className="nav-menu">
         <li onClick={()=>{setMenu("shop")}}><Link style={{textDecoration:'none'}} to="/">Shop</Link>{menu==="shop"?<hr/>:<></>}</li>
         <li onClick={()=>{setMenu("mens")}}><Link style={{textDecoration:'none'}} to="/mens">Men</Link>{menu==="mens"?<hr/>:<></>}</li>
