eccab463f07a9f763f0e95bfcf3c2e10a39d329d
ckorir <chrisruto.k@gmail.com>
2024-07-20 13:32:09 +0300

    Created upload endpoint for images

--- backend/index.js
+++ backend/index.js
@@ -30,6 +30,20 @@ const storage = multer.diskStorage({
     }
 });
 
+const upload = multer({
+    storage: storage
+});
+
+// Create upload endpoint for Images
+app.use('/images', express.static('upload/images'));
+
+app.post('/upload', upload.single('product'), (req, res) => {
+    res.json({
+        success: 1,
+        image_url: `http://localhost:${port}/images/${req.file.filename}`
+    });
+});
+
 app.listen(port, (error) => {
     if(error) {
         console.log("error" + error);
