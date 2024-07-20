6458489b7ecd54c59ef01d4c1a2f5396471c5501
ckorir <chrisruto.k@gmail.com>
2024-07-20 12:59:21 +0300

    Added connection to Mongo Db and API listening

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,5 @@
+// Intitialize Dependencies
+
 const port = 4000;
 const express = require('express');
 const app = express();
@@ -6,3 +8,21 @@ const jwt = require('jsonwebtoken');
 const multer = require('multer');
 const path = require('path');
 const cors = require('cors');
+
+app.use(express.json());
+app.use(cors());
+
+// Connect to MongoDB
+mongoose
+    .connect('mongodb+srv://korir:Kipkorir321@cluster0.6n9ehqk.mongodb.net/e-commerse')
+    .then(() => console.log('MongoDB Connected...'))
+    .catch(err => console.log(err));
+
+// API Creation
+app.listen(port, (error) => {
+    if(error) {
+        console.log(error);
+    } else {
+        console.log("Server is running on port: " + port);
+    }
+})
