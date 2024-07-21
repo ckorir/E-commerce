1689c81a63b9d8b5f2c29fab992625d746158f60
ckorir <chrisruto.k@gmail.com>
2024-07-21 16:09:09 +0300

    Added Routes in Admin for Adding & Listing products

---
+++ admin/src/components/listProduct/ListProduct.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../../css/ListProduct.css'
+
+const ListProduct = () => {
+  return (
+    <div className='listproduct'>
+        
+    </div>
+  )
+}
+
+export default ListProduct
--- admin/src/pages/admin/Admin.jsx
+++ admin/src/pages/admin/Admin.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import '../../css/Admin.css';
 import Sidebar from '../../components/sidebar/Sidebar';
+import { Routes, Route } from 'react-router-dom';
+import AddProduct from '../../components/addProduct/AddProduct';
+import ListProduct from '../../components/listProduct/ListProduct';
 
 const Admin = () => {
-  return (
+  return ( 
     <div className="admin">
       <Sidebar />
+      <Routes>
+        <Route path="/addproduct" element={<AddProduct />} />
+        <Route path="/listproduct" element={<ListProduct />} />
+      </Routes>
     </div>
   );
 };
