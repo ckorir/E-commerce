cab08a2052a2f187b45a64cb9d98e64db9e1bf19
ckorir <chrisruto.k@gmail.com>
2024-07-23 11:44:29 +0300

    Created login state

--- frontend/src/pages/LoginSingup.js
+++ frontend/src/pages/LoginSingup.js
@@ -1,17 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../css/LoginSingup.css'
 
 export default function LoginSingup() {
+
+  const [state,setState] = useState("Login")
+
   return (
     <div className='loginsingup'>
       <div className='loginsingup-container'>
-        <h1>Sign Up</h1>
+        <h1>{state}</h1>
         <div className='loginsingup-fields'>
-          <input type="text" placeholder='Username' />
+          {state === "Signup" ? <input type="text" placeholder='Username' />:<></>} 
           <input type="email" placeholder='Email Address' />
           <input type="password" placeholder='Password' />
           <button>Continue</button>
-          <p className='loginsingup-login'>Already have an account?<span> Login </span></p>
+          {state === "Signup" ? <p className='loginsingup-login'>Already have an account?<span> Login </span></p>
+          :<p className='loginsingup-login'>Create an account<span> Signup </span></p>}
           <div className='loginsingup-agree'>
             <input type="checkbox" name='' id='' />
             <p>I agree to the <span>Terms & Conditions</span></p>
