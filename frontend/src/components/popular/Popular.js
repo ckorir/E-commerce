7de1f25026eb073a8c48e96a5a75ad926229cc75
ckorir <chrisruto.k@gmail.com>
2024-07-24 11:47:44 +0300

    Connected popular to frontend

--- frontend/src/components/popular/Popular.js
+++ frontend/src/components/popular/Popular.js
@@ -1,15 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import '../../css/Popular.css'
 import data_product from '../Assets/data'
 import Item from '../item/Item'
 
 export default function Popular() {
+
+  const [popular,setPopular] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/popular")
+    .then((res) => res.json())
+    .then((data) => {
+        setPopular(data);
+    });
+  }, []);
+
   return (
     <div className='popular'>
       <h1>POPULAR IN WOMEN</h1>
       <hr/>
       <div className='popular-item'>
-        {data_product.map((item,i)=>{
+        {popular.map((item,i)=>{
           return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price} />
         })}
       </div>
