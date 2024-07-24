23d4f4eaae714a8d6f3564399de7fdef271505ae
ckorir <chrisruto.k@gmail.com>
2024-07-24 11:10:32 +0300

    Connected newcollections API to frontend

--- frontend/src/components/newCollections/NewCollections.js
+++ frontend/src/components/newCollections/NewCollections.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import '../../css/NewCollections.css'
-import new_collections from '../Assets/new_collections'
+// import new_collections from '../Assets/new_collections'
 import Item from '../item/Item'
 
 export default function NewCollections() {
+
+  const [new_collections,setNewCollections] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/newcollection")
+    .then((res) => res.json())
+    .then((data) => {
+        setNewCollections(data);
+    });
+  }, []);
+
   return (
     <div className='new-collections'>
         <h1>NEW COLLECTIONS</h1>
